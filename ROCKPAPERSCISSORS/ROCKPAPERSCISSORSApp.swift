a2ccc42b4352161b6ac0630bbdf66cf2a61e4d3a
Drake Wright <rdrakewright@gmail.com>
2023-05-01 08:43:34 -0500

    Initial Commit

---
+++ ROCKPAPERSCISSORS/ROCKPAPERSCISSORSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROCKPAPERSCISSORSApp.swift
+//  ROCKPAPERSCISSORS
+//
+//  Created by Drake Wright on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ROCKPAPERSCISSORSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
